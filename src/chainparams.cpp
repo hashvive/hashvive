d43fd972cc97ffd0e34c217e21449bb8d6ea39cc
Adler <hi@adlerlopes.com>
2025-01-09 00:24:59 -0600

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -22,9 +22,9 @@ static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits
 {
     const char* pszTimestamp = "The Hashvive blockchain starts now!";
     const CScript genesisOutputScript = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
-    
+
     CMutableTransaction txNew;
-    txNew.nVersion = 1;
+    txNew.version = 1;
     txNew.vin.resize(1);
     txNew.vout.resize(1);
     txNew.vin[0].scriptSig = CScript() << 486604799 << CScriptNum(4) << std::vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
@@ -57,7 +57,7 @@ public:
         consensus.CSVHeight = 0;
         consensus.SegwitHeight = 0;
         consensus.MinBIP9WarningHeight = 0;
-        consensus.powLimit = uint256S("00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+        consensus.powLimit = uint256("00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // 2 semanas
         consensus.nPowTargetSpacing = 5 * 60;             // 5 minutos
         consensus.fPowAllowMinDifficultyBlocks = false;
@@ -68,8 +68,8 @@ public:
         // Genesis block
         genesis = CreateGenesisBlock(1736391250, 3130497225, 0x1d00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0000000076798936e23559c7cf6cb7a5175331047889b155496a2bbd4c94e900"));
-        assert(genesis.hashMerkleRoot == uint256S("96fb97a45f46a11eb4059325cd5c834186eb35eda1b78021bab19b249578fa50"));
+        assert(consensus.hashGenesisBlock == uint256("0000000076798936e23559c7cf6cb7a5175331047889b155496a2bbd4c94e900"));
+        assert(genesis.hashMerkleRoot == uint256("96fb97a45f46a11eb4059325cd5c834186eb35eda1b78021bab19b249578fa50"));
 
         pchMessageStart[0] = 0x68; // 'h'
         pchMessageStart[1] = 0x76; // 'v'
@@ -88,13 +88,9 @@ public:
 
         bech32_hrp = "hv";
 
-        fDefaultConsistencyChecks = false;
-        fRequireStandard = true;
-        fMineBlocksOnDemand = false;
-
         checkpointData = {
             {
-                {0, uint256S("0000000076798936e23559c7cf6cb7a5175331047889b155496a2bbd4c94e900")},
+                {0, uint256("0000000076798936e23559c7cf6cb7a5175331047889b155496a2bbd4c94e900")},
             }
         };
 
